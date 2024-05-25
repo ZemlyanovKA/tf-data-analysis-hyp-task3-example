616f258238af612b03a80324604c19541d7ea1d3
ZemlyanovKA <70999569+ZemlyanovKA@users.noreply.github.com>
2024-05-25 21:07:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 527952713 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_group: np.array,
+             test_group: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.09
+    # Рассчитываем t-статистику и p-значение
+    t_stat, p_value = ttest_ind(control_group, test_group, equal_var=False)  # Используем Welch's t-test
+    
+    # Сравниваем p-значение с уровнем значимости
+    if p_value < alpha:
+        return True  # Отклоняем нулевую гипотезу
+    else:
+        return False  # Не отклоняем нулевую гипотезу
